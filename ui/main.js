a5bdf47176e6df6acceefb683a4ab351c62f2e2f
Malekulashtar <36704518+Malekulashtar@users.noreply.github.com>
2018-04-04 22:48:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,6 @@
-console.log('Loaded!');
-var element=document.getElementById('main_text');
-element.innerHTML='New value';
-
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveright()
-{
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';
+var button=document.getElementById('counter');
+button.onclick=function(){
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
 }
-img.onclick=function()
-{
-    var interval=setInterval(moveright,50)
-};
