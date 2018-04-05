d0a9d6e04ea09f2a6c2271b6ed12c614b00f1c96
Malekulashtar <36704518+Malekulashtar@users.noreply.github.com>
2018-04-05 22:10:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.get('/counter',function(req,res){
 });
 
 var names=[];
-app.get('/submit-name/nmae',function(req,res){
+app.get('/submit-name/:nmae',function(req,res){
     var name=req.param.name;
     names.push(name);
     res.send(JSON.stringify(names));
