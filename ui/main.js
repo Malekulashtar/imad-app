e87ae91f6b12db723d2be8fcc069569ca5386948
Malekulashtar <36704518+Malekulashtar@users.noreply.github.com>
2018-04-05 21:27:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,3 +19,17 @@ button.onclick=function(){
     request.open('GET','http://afmul53.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+
+var nameinput=document.getElementById('name');
+var name=nameinput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick=function()
+{
+    var names=[name1,name2,name3,name4];
+    var list='';
+    for(var i=0;i<names.length;i++){
+        list+='<li>'+names[i]+'</li>';
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML=list;
+};
