d969be2ad63e25ed25ce8ea0f523cc111e7d6b0e
Malekulashtar <36704518+Malekulashtar@users.noreply.github.com>
2018-04-12 00:19:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -124,7 +124,7 @@ app.get('/articles/:articlename', function (req, res){
        else{
            if(result.rows.length===0)
            {
-               res.status(404).send('article not found')
+               res.status(404).send('article not found');
            }
            else
            {
