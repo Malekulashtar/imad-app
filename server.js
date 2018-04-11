fec369b97b9f800e7cc5a609de0a111606080c2b
Malekulashtar <36704518+Malekulashtar@users.noreply.github.com>
2018-04-12 00:33:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.get('/', function (req, res) {
 
 
 var pool=new pool(config);
-app.get('/articles/:articleName', function (req, res){ 
+app.get('/article/:articleName', function (req, res){ 
 pool.query("SELECT * FROM article WHERE title='"+req.params.articleName+"'",function(err,result){
        if(err){res.status(500).send(err,toString());}
        else{
