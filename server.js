92a9c3aacfe416352fdd81969fee897d45483331
Malekulashtar <36704518+Malekulashtar@users.noreply.github.com>
2018-04-12 00:43:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var pool=require('pg').pool;
+var Pool=require('pg').Pool;
 
 var config={
     user:'afmul53',
@@ -84,7 +84,7 @@ app.get('/', function (req, res) {
 });
 
 
-var pool=new pool(config);
+var Pool=new Pool(config);
 app.get('/articles/:articleName', function (req, res){ 
 pool.query("SELECT * FROM article WHERE title='"+req.params.articleName+"'",function(err,result){
        if(err){
